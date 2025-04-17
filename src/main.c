b35707324641f7f167104ea9adc5664e2396de65
Cyril-Glitch <cyrilcolonneau@hotmail.fr>
2025-04-17 14:03:20 +0200

    maj wasd

--- src/main.c
+++ src/main.c
@@ -10,64 +10,67 @@
 
 int main( int agrc , const char argv [])
 {
+	//initialisation SDL + Fenetre
 	int true = 1;
 
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
 
-//initilisation de la sdl , de la fenetre puis du rendu et enfin de la texture
 
 	cc2d_init();
 	cc2d_init_window("window test" ,2000,1000,&renderer,&window);
 	
+	//intialisation graphics
 	SDL_Texture* texplanet = cc2d_loadImage(renderer,"../img/planet.png");
+	int xp = 0;
+	int yp = 0;
 
+	//initialisation font et textes
 	TTF_Font* font = cc2d_loadFont("../font/PixelMaster.ttf",100);
 	SDL_Texture* titre = cc2d_textureTexte("Hello World !",renderer,font,100,100,255,255,255,255);
-	int x = 0;
+	
 
 
+	//depart de la gameloop
 	while(true)
 	{
 		
-		SDL_SetRenderDrawColor(renderer,0,0,10,255);//initialise le render en bleu
-		if(cc2d_beginDraw(renderer) == 0 )//efface le renderer
+		SDL_SetRenderDrawColor(renderer,0,0,10,255);        //initialise le render en bleu
+		if(cc2d_beginDraw(renderer) == 0 )                  //efface le renderer
 		{
 			break;
 		}
 		
 		//chargement du contenue
 
-		SDL_SetRenderDrawColor(renderer,255,0,255,255); 
+		SDL_SetRenderDrawColor(renderer,255,0,255,255);      //dessine la ligne
 		SDL_RenderDrawLine(renderer,0,0,2000,1000);
 
-		SDL_SetRenderDrawColor(renderer,0,255,255,255); 
-		cc2d_drawRect(renderer,"line",1000,500,100,100);
-		cc2d_drawRect(renderer,"fill",1100,600,100,100);	
-		SDL_SetRenderDrawColor(renderer,0,255,255,122); 
-		cc2d_drawRect(renderer,"fill",1100,650,100,100);
-		
-		cc2d_Draw(texplanet,renderer,x,10,150,150,255);
+		cc2d_Draw(texplanet,renderer,xp,yp,150,150,255);     //affiche la planet 
 		cc2d_Draw(titre,renderer,300,500,200,100,255);
-
-/*		const Uint8* state = SDL_GetKeyboardState(NULL);
-		if(state[SDL_SCANCODE_D])
-		{
-			x++;
-		}
-
-*/
 
 
 		if(cc2d_downKey(SDL_SCANCODE_D))
 		{
-			x++;
+			xp++;
+		}
+		if(cc2d_downKey(SDL_SCANCODE_A))
+		{
+			xp--;
+		}
+		if(cc2d_downKey(SDL_SCANCODE_S))
+		{
+			yp++;
+		}
+		if(cc2d_downKey(SDL_SCANCODE_W))
+		{
+			yp--;
 		}
 
-		cc2d_enddraw(renderer);//affiche le resultat
+		cc2d_enddraw(renderer);    //affiche le resultat
 
 
 	}
 
-	cc2d_close(window,renderer);//libere la memoire
+	cc2d_close(window,renderer);       //libere la memoire
 }
